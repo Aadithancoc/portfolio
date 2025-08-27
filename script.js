31e3c60617b95cdd6c91932eb9d04fa5e6a44dfe
Aadithancoc <alaguaadithan2656@gmail.com>
2025-08-27 12:23:11 +0530

    Updated portfolio

--- script.js
+++ script.js
@@ -18,3 +18,9 @@ const navLinks = document.getElementById('nav-links');
 menuToggle.addEventListener('click', () => {
   navLinks.classList.toggle('show');
 });
+
+document.querySelectorAll('.nav-links a').forEach(link => {
+  link.addEventListener('click', () => {
+    navLinks.classList.remove('show');
+  });
+});
